4c36f560307f21dd75aedfd4b2780763dbb000f7
Ray Yu <kaito-akira@hotmail.com>
2022-10-11 18:02:35 -0700

    counter completed

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+  $('.tweet-text').on("input", onInput);
+
+});
+
+
+const onInput = function () {
+  const $input = $(this);
+  const text = $input.val();
+  
+  let $counter = $(this).parent().find(".counter");
+  
+  const remaining = 140 - text.length;
+  
+  if (remaining < 0) {
+    $($counter).addClass("counter-red");
+  } else {
+    $($counter).removeClass("counter-red");
+  }
+
+  $counter.text(remaining)
+
+};
+
